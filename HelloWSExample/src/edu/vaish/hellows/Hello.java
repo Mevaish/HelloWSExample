6c9f36b93ebe5db55e29ea8ec3990b5ecb6184f3
mvaish <vaishalimudkhedkars@gmail.com>
2015-04-13 08:16:44 +0530

    Initial Commit

---
+++ HelloWSExample/src/edu/vaish/hellows/Hello.java
@@ -0,0 +1,16 @@
+package edu.vaish.hellows;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@  WebService
+public class Hello {
+	private String message = new String("Hello ");
+	public void Hello(){
+	}
+	@WebMethod
+	public String sayHello(String name){
+		return message+name +".";
+	}
+	
+}
